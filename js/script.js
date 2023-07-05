8aa427f83b7067a7803d98f2a4c961ce698b75c6
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-07-05 16:02:18 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
-$(function (){
-    var paramName = "parameter";//参数名
-    var tag = '';//标记值
-	//默认参数值
-	var parameter = getQueryString(paramName);
-	if(parameter == ''){//默认参数值为空
-        var paramArray = [1,2,3,4,5];//默认存在业务来源参数数组
-        for (var index = 0; index < paramArray.length; index++) {
-            tag = paramArray[index];
-            parameter = getQueryString(paramName + tag);
-            if(parameter != ''){
-                sendMessageTag(parameter,tag);
-                break;//直接退出
-            }
-        }
-        //默认的request请求即没有业务数据
-        if(parameter == ''){
-            sendMessageRequest();
-        }
-	}else{
-	    //默认的参数-无法识别来源
-        sendMessageTag(parameter,tag);
-	}
-
-	
+$(function (){	
     function doSuit(el, pageWidth, pageHeight) {
         var h = pageHeight
         var w = pageWidth
@@ -58,43 +34,4 @@ function getQueryString(name){
 		return  r[2]; 
 	 }  
 	 return ''; 
-}
-
-//pararmter
-function sendMessageTag(parameter,tag) {
-    try {
-        $.ajax({
-            url: "/warning",
-            type: "POST",
-            data: {parameter: parameter, tag: tag},
-            success: function (data) {
-                //console.log('success',data)
-            },
-            error: function (err) {
-                //console.log('fail',err)
-            }
-        });
-    } catch (e) {
-        console.log('fail', e)
-    }
-}
-
-//request
-function sendMessageRequest(){
-	try {
-
-		$.ajax({
-			url:"/warning",
-			type:"GET",
-            cache: false,
-			success:function(data){
-			//console.log('success',data)
-			},
-            error:function(err) {
-               //console.log('fail',err)
-            }
-		});	 
-	} catch (e) {
-		console.log('fail',e)
-	}
 }
